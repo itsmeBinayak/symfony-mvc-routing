194f9026c8ddb3e287c9c247fe32636f557ed54a
binayak <69388246+itsme17@users.noreply.github.com>
2022-01-12 16:51:57 +0100

    comment edited

--- src/service/Logger.php
+++ src/service/Logger.php
@@ -5,7 +5,7 @@ class Logger{
     public function logger(string $msg)
     {
         $msg .= "\n";
-        // Write the contents to the file, 
+        // Write the contents back to the file
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
         file_put_contents('log.info',$msg , FILE_APPEND);
